80040a2012647a2bf3050dd83fa7041379188ce9
yamagami2211 <mail@yama2211.jp>
2021-12-17 16:01:59 +0900

    Rulesを追加

---
+++ Rules/language/Japanese/rules.php
@@ -0,0 +1,50 @@
+<?php 
+/*
+ *	Rules module made by Coldfire
+ *	https://coldfiredzn.com
+ *
+ *	Using code from the vote module by Partydragen and Samerton
+ */
+
+$language = array( 
+	'rules_buttons' => 'ボタン',
+	'rules_button_name' => 'ボタン名',
+	'no_rules_buttons' => 'ボタンが定義されていません',
+	'rules_button_link' => 'ボタンリンク',
+	'new_button' => '新しいボタン',
+	'edit_button' => 'ボタンの編集',
+	'delete_button' => 'このボタンを削除してもよろしいですか？',
+	'button_created_successfully' => 'ボタンが正常に作成されました。',
+	'button_updated_successfully' => 'ボタンが正常に更新されました。',
+	'button_deleted_successfully' => 'ボタンが正常に削除されました。',
+	'button_name_required' => 'ボタン名が必要です。',
+	'button_link_required' => 'ボタンリンクが必要です。',
+	'button_name_minimum' => 'ボタン名は1文字以上にする必要があります。',
+	'button_link_minimum' => 'ボタンリンクは1文字以上である必要があります。',
+	'button_name_maximum' => 'ボタン名は最大96文字である必要があります。',
+	'button_link_maximum' => 'ボタンリンクは最大96文字である必要があります。',
+	'rules' => 'ルール',
+	'rules_catagories' => 'カテゴリ',
+	'rules_catagory_name' => 'カテゴリー名',
+	'no_rules_catagories' => 'カテゴリが定義されていません',
+	'rules_catagory_icon' => 'カテゴリアイコン(無効にするには空白のままにします)',
+	'rules_catagory_rules' => 'ルール',
+	'new_catagory' => '新しいカテゴリ',
+	'edit_catagory' => 'カテゴリの編集',
+	'delete_catagory' => 'このカテゴリを削除してもよろしいですか？',
+	'message' => 'ルールページのホームタブに表示するテキスト',
+	'message_maximum' => 'エラーが発生した。 ルールメッセージは必須であり、最大2048文字である必要があります。 アイコンは最大64文字である必要があります。',
+	'link_location' => 'リンクの場所',
+	'icon' => 'アイコン',
+	'rules_action' => 'アクション',
+	'icon_example' => '例: <i class="fas fa-gavel"></i>',
+	'catagory_created_successfully' => 'カテゴリが正常に作成されました。',
+	'catagory_updated_successfully' => 'カテゴリが正常に更新されました。',
+	'catagory_deleted_successfully' => 'カテゴリが正常に削除されました。',
+	'catagory_name_required' => 'カテゴリ名は必須です。',
+	'catagory_rules_required' => 'ルールリストが必要です。',
+	'catagory_name_minimum' => 'カテゴリ名は1文字以上にする必要があります。',
+	'catagory_rules_minimum' => 'ルールリストは1文字以上である必要があります。',
+	'catagory_name_maximum' => 'カテゴリ名は最大96文字である必要があります。',
+	'catagory_icon_maximum' => 'カテゴリアイコンは最大96文字である必要があります。'
+);
