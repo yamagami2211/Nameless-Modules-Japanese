66fadfad610a6857c48cbb18aec3fccb250021ea
yamagami2211 <mail@yama2211.jp>
2021-12-23 00:07:58 +0900

    Staffを追加

---
+++ Staff/language/Japanese/general.php
@@ -0,0 +1,40 @@
+<?php 
+
+/**
+ *	STAFF PAGE MODULE
+ *	By Xemah | https://xemah.com
+ *
+**/
+
+$language = [
+
+	'title' => 'スタッフ',
+	'settings' => 'スタッフページの設定',
+
+	'permission' => 'パネル > スタッフページの設定',
+
+	'panel' => 'パネル',
+	'dashboard' => 'ダッシュボード',
+
+	'pageTitle' => 'ページリンク',
+	'linkPath' => 'リンクパス',
+	'linkLocation' => 'リンクの場所',
+	'navbar' => 'ナビゲーションバー',
+	'navbarMoreDropdown' => '"さらに"ドロップダウン',
+	'footer' => 'フッター',
+	'none' => 'なし',
+	'navIcon' => 'ナビゲーションアイコン',
+	'navOrder' => 'ナビゲーション順序',
+
+	'submit' => '送信',
+	'cancel' => 'キャンセル',
+
+	'settingsUpdated' => '設定が正常に更新されました。',
+	
+	'success' => '成功',
+	'error' => 'エラー',
+
+	'errorEdit' => 'フィールドが正しく入力されていることを確認してください。',
+	'errorToken' => '無効なトークン！ もう一度やり直してください。',
+
+];
---
+++ Staff/language/Readme.md
@@ -0,0 +1,6 @@
+# Rules
+モジュールのダウンロードは以下のリンクから。  
+https://namelessmc.com/resources/resource/65-staff--a-free-namelessmc-module/
+
+# 作成
+やまがみ [Twitter](https://twitter.com/yamagami2211_02)
